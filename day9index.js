4ac3bd6391b1a9cde9231899a751e6fa6eeff3fa
Ashishkumar-NIT <ashishkumar05164@gmail.com>
2026-04-13 19:07:21 +0530

    function declaration and function call

---
+++ day9index.js
@@ -0,0 +1,18 @@
+// how functions are declared
+
+function greeting(){
+    console.log("Hello world!");
+}
+
+// function call
+greeting();
+
+// funcitons with arguments and parameters
+function addnumber(param1,param2){
+    let sum = param1 + param2;
+    console.log(`The sum of ${param1} & ${param2} : ${sum}`);
+}
+addnumber(12,34);
+// The sum of 12 & 34 : 46
+addnumber(532,3344);
+// The sum of 532 & 3344 : 3876
